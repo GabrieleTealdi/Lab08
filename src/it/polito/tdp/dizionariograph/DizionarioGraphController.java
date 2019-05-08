f85dadc6bec8a15d90b4656e6c08a566e332ef8e
gabrieletealdi <gabrieletealdi@mbp-di-gabriele.homenet.telecomitalia.it>
2019-05-08 20:53:58 +0200

    problema quasi completato. Mancano setDisable() bottoni e txtField

--- src/it/polito/tdp/dizionariograph/DizionarioGraphController.java
+++ src/it/polito/tdp/dizionariograph/DizionarioGraphController.java
@@ -1,5 +1,80 @@
 package it.polito.tdp.dizionariograph;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import it.polito.tdp.dizionariograph.model.Model;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class DizionarioGraphController {
 	
+	private Model model;
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField txtNumLettere;
+
+    @FXML
+    private TextField txtParola;
+
+    @FXML
+    private TextArea txtResult;
+
+    @FXML
+    void doCreaGrafo(ActionEvent event) {
+    	int numLettere = Integer.parseInt(txtNumLettere.getText());
+    	this.model.createGraph(numLettere);
+    	txtResult.appendText(String.format("Grafo creato: %d vertici e %d archi\n", 
+    			model.getGraph().vertexSet().size(), model.getGraph().edgeSet().size()));
+    	
+    }
+
+    @FXML
+    void doReset(ActionEvent event) {
+    	txtNumLettere.clear();
+    	txtParola.clear();
+    	txtResult.clear();
+    }
+
+    @FXML
+    void doTrovaGradoMax(ActionEvent event) {
+    	txtResult.appendText(this.model.findMaxDegree());
+    }
+
+    @FXML
+    void doTrovaVicini(ActionEvent event) {
+    	String parola = txtParola.getText();
+    	try {
+    		if(parola.length()==0 || parola==null) {
+    			txtResult.appendText("parola inserita non valida");
+    			return;
+    		}
+    		txtResult.appendText(String.format("Neighbours di %s:\n", parola));
+    		txtResult.appendText(this.model.displayNeighbours(parola).toString()+"\n");
+    	}catch(RuntimeException re) {
+    		txtResult.appendText(re.getMessage());
+    	}
+    }
+
+    @FXML
+    void initialize() {
+        assert txtNumLettere != null : "fx:id=\"txtNumLettere\" was not injected: check your FXML file 'DizionarioGraph.fxml'.";
+        assert txtParola != null : "fx:id=\"txtParola\" was not injected: check your FXML file 'DizionarioGraph.fxml'.";
+        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'DizionarioGraph.fxml'.";
+
+        
+        
+    }
+    
+    public void setModel(Model model) {
+    	this.model = model;
+    }
 }
--- src/it/polito/tdp/dizionariograph/model/Model.java
+++ src/it/polito/tdp/dizionariograph/model/Model.java
@@ -3,21 +3,94 @@ package it.polito.tdp.dizionariograph.model;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.jgrapht.Graph;
+import org.jgrapht.Graphs;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.SimpleGraph;
+
+import it.polito.tdp.dizionariograph.db.WordDAO;
+
 public class Model {
+	
+	private Graph<String, DefaultEdge> graph;
+	private int numLettere = 0;
+	private List<String> parole;
 
 	public void createGraph(int numeroLettere) {
 
-		System.err.println("createGraph -- TODO");
+		graph = new SimpleGraph<>(DefaultEdge.class);
+		WordDAO dao = new WordDAO();
+		this.numLettere = numeroLettere;
+		this.parole = dao.getAllWordsFixedLength(numeroLettere);
+		
+		Graphs.addAllVertices(graph, this.parole);
+		
+		
+		for(String parola: parole) {
+			List<String> paroleSimili = trovaSimili(parola);
+			for(String ps: paroleSimili)
+				graph.addEdge(parola, ps);
+		}
+		
+		//System.out.println(graph);
+		
 	}
+
 
 	public List<String> displayNeighbours(String parolaInserita) {
 
-		System.err.println("displayNeighbours -- TODO");
-		return new ArrayList<String>();
+		if(!parole.contains(parolaInserita))
+			throw new RuntimeException("parola non esistente nel vocabolario");
+		if(parolaInserita.length()!=numLettere)
+			throw new RuntimeException("la parola non coincide con il numero di lettere inserite");
+		
+		return Graphs.neighborListOf(graph, parolaInserita);
 	}
 
-	public int findMaxDegree() {
-		System.err.println("findMaxDegree -- TODO");
-		return -1;
+	public String findMaxDegree() {
+		
+		String record = null;
+		int gradoRecord = 0;
+		
+		for(String parola: graph.vertexSet()) {
+			int grado = Graphs.neighborListOf(graph, parola).size();
+			if(grado>gradoRecord) {
+				record = parola;
+				gradoRecord = Graphs.neighborListOf(graph, parola).size();
+			}
+		}
+		
+		if(gradoRecord!=0) {
+			StringBuilder sb = new StringBuilder();
+			sb.append(String.format("Massimo grado: %d\nVertice con Massimo grado: %s\n", gradoRecord, record));
+			sb.append(String.format("Neighbours di %s: \n", record));
+			for(String s: Graphs.neighborListOf(graph, record)) {
+				sb.append(s +"\n");
+			}
+			return sb.toString();
+		}
+		
+		return null;
 	}
+	
+
+	private List<String> trovaSimili(String parola) {
+		List<String> simili = new ArrayList<>();
+		for(String s: parole) {
+			int diff = 1;
+			for(int i=0; i<numLettere; i++) {
+				if(s.charAt(i)!=parola.charAt(i))
+					diff--;
+			}
+			if(diff==0)
+				simili.add(s);	
+		}
+		return simili;
+	}
+
+
+	public Graph<String, DefaultEdge> getGraph() {
+		return graph;
+	}
+	
 }
